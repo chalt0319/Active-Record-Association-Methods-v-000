781ce36d6145784415e0614b4a9d2406dc53d1ca
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-20 15:18:04 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -12,6 +12,6 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    self.songs.genres.length
+    self.genres.length
   end
 end
